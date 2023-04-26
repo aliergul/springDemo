94c99e4d463251151ecac9325a3608033c7a31a1
aliergul <m.alieergul@gmail.com>
2023-04-26 16:55:01 +0300

    İş kodları eklendi.

--- springDemo/src/main/java/kodlamaio/springDemo/business/abstracts/ProductService.java
+++ springDemo/src/main/java/kodlamaio/springDemo/business/abstracts/ProductService.java
@@ -2,12 +2,27 @@ package kodlamaio.springDemo.business.abstracts;
 
 import java.util.List;
 
+
 import kodlamaio.springDemo.core.utilities.results.DataResult;
 import kodlamaio.springDemo.core.utilities.results.Result;
 import kodlamaio.springDemo.entities.concretes.Product;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
-	
 	Result add(Product product);
+	
+	
+	DataResult <Product> getByProductName(String productName);
+	
+	DataResult <Product> getByProductNameAndCategoryId(String productName, int categoryId);
+	
+	DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId);
+
+	DataResult<List<Product>> getByCategoryIdIn(List <Integer> categories);
+	
+	DataResult<List<Product>> getByProductNameContains(String productName);
+
+	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+	
+	DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
 }
